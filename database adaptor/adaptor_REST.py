0e42657e0a6aa39ddd2fad6cabb54e8f437efd44
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2021-12-08 22:40:30 +0100

    Update adaptor_REST.py

--- database adaptor/adaptor_REST.py
+++ database adaptor/adaptor_REST.py
@@ -10,7 +10,31 @@ class AdaptorREST():
     def __init__(self,conf_filename):
         self.adaptor=Adaptor(conf_filename)
         self.service=self.adaptor.registerRequest()
-    
+
+    def GET(self,*uri,**params):
+        try:
+            platform_ID=uri[0]
+            #room_ID=uri[1]
+            command=uri[1]
+            try:
+                clients_catalog=self.adaptor.retrieveService('clients_catalog')
+                clients_result=requests.get(clients_catalog['url']+"/info/"+platform_ID+"/thingspeak").json()
+                channelID=clients_result["channelID"]
+            except Exception as e:
+                print(e)
+                raise cherrypy.HTTPError(500,"Ops! Try later..")
+
+        except:
+            raise cherrypy.HTTPError(400,"Check your request and try again!")
+        if command=="now":
+            thingspeak_url="{}/channels/{}/feeds.json?results=1".format(self.adaptor.thingspeak_url,channelID)
+            result=requests.get(thingspeak_url).json()
+            print(result)
+            return json.dumps(result)
+        else:
+            raise cherrypy.HTTPError(501, "No operation!")
+
+
         
 if __name__ == '__main__':
     conf=sys.argv[1]
@@ -26,6 +50,6 @@ if __name__ == '__main__':
         cherrypy.config.update({'server.socket_host': adaptorREST.adaptor.serviceIP})
         cherrypy.config.update({'server.socket_port': adaptorREST.adaptor.servicePort})
         cherrypy.engine.start()
-        
+
         adaptorREST.adaptor.setup("adaptor")
         cherrypy.engine.block()
