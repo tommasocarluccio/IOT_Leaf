4313b2c24ccdd67d59f7c5a3323486ea0d89357f
Vincenzo Madaghiele <vincenzo.madaghiele@gmail.com>
2021-12-12 20:08:31 +0100

    Update statistics_service.py

--- statistics_service.py
+++ statistics_service.py
@@ -7,20 +7,11 @@ import numpy as np
 from datetime import datetime
 from dateutil.relativedelta import relativedelta 
 
-def retrieveData(channelID, period='month'):
-
-	# check dates
-	now = datetime.now()
-	if period == 'day':
-		last_period_date = now + relativedelta(days=-1)
-	elif period == 'week':
-		last_period_date = now + relativedelta(weeks=-1)
-	elif period == 'month':
-		last_period_date = now + relativedelta(months=-1)
+def retrieveData(channelID, date_start, date_end):
 	
 	# format dates
-	date_now = now.strftime("%Y-%m-%d %H:%M:%S").split()
-	date_last = last_period_date.strftime("%Y-%m-%d %H:%M:%S").split()
+	date_now = date_start.strftime("%Y-%m-%d %H:%M:%S").split()
+	date_last = date_end.strftime("%Y-%m-%d %H:%M:%S").split()
 	end_date = '%20'.join(date_now)
 	start_date = '%20'.join(date_last)
 
@@ -30,7 +21,7 @@ def retrieveData(channelID, period='month'):
 
 	return res.json()
 
-def calculateAvgs(json_response):
+def calculateStats(json_response):
 
 	# get element into list
 	AQI = []
@@ -49,16 +40,174 @@ def calculateAvgs(json_response):
 	temp = temp[~(np.isnan(temp))]
 	hum = hum[~(np.isnan(hum))]
 
-	return AQI.mean(), temp.mean(), hum.mean()
+	resp = {}
+	resp['AQI'] = {}
+	resp['temp'] = {}
+	resp['hum'] = {}
+	if AQI.size > 0:
+		resp['AQI']['avg'] = AQI.mean()
+		resp['AQI']['max'] = AQI.max()
+		resp['AQI']['min'] = AQI.min()
+	else:
+		resp['AQI']['avg'] = 'no_data'
+		resp['AQI']['max'] = 'no_data'
+		resp['AQI']['min'] = 'no_data'
+	if temp.size > 0:
+		resp['temp']['avg'] = temp.mean()
+		resp['temp']['max'] = temp.max()
+		resp['temp']['min'] = temp.min()
+	else:
+		resp['temp']['avg'] = 'no_data'
+		resp['temp']['max'] = 'no_data'
+		resp['temp']['min'] = 'no_data'
+	if hum.size > 0:
+		resp['hum']['avg'] = hum.mean()
+		resp['hum']['max'] = hum.max()
+		resp['hum']['min'] = hum.min()
+	else:
+		resp['hum']['avg'] = 'no_data'
+		resp['hum']['max'] = 'no_data'
+		resp['hum']['min'] = 'no_data'
+
+	return resp
 
 if __name__ == '__main__':
 
 	# request data to thingspeak
 	channelID = '1021300'
-	period = 'month' # day OR week OR month
-	res = retrieveData(channelID, period)
+	period = 'week' # day OR week OR month
 
-	# calculate avgs
-	AQI_avg, temp_avg, hum_avg = calculateAvgs(res)
-	print(AQI_avg, temp_avg, hum_avg)
+	# check dates
+	now = datetime.now()
+	if period == 'day':
+		last_period_date = now + relativedelta(days=-1)
+		res = retrieveData(channelID, now, last_period_date)
+		respDEF = calculateStats(res)
+
+		NUM_DAYS = 7
+		avg_lastAQI = 0
+		avg_lastTemp = 0
+		avg_lastHum = 0
+
+		try:
+			# query for avgs of last 7 days
+			for d in range(NUM_DAYS):
+				now = last_period_date
+				last_period_date = now + relativedelta(days=-1)
+				res = retrieveData(channelID, now, last_period_date)
+				resp = calculateStats(res)
+				avg_lastAQI += resp['AQI']['avg']
+				avg_lastTemp += resp['temp']['avg']
+				avg_lastHum += resp['hum']['avg']
+			avg_lastAQI /= NUM_DAYS
+			avg_lastTemp /= NUM_DAYS
+			avg_lastHum /= NUM_DAYS
+
+			# print advice msg
+			if respDEF['AQI']['avg'] > avg_lastAQI:
+				AQI_avice = f'The average AQI today is higher than the previous {NUM_DAYS} days! (avg: {avg_lastAQI})'
+			else:
+				AQI_avice = f'The average AQI today is lower than the previous {NUM_DAYS} days! (avg: {avg_lastAQI})'
+			if respDEF['temp']['avg'] > avg_lastTemp:
+				temp_avice = f'The average temperature today is higher than the previous {NUM_DAYS} days! (avg: {avg_lastTemp})'
+			else:
+				temp_avice = f'The average temperature today is lower than the previous {NUM_DAYS} days! (avg: {avg_lastTemp})'
+			if respDEF['hum']['avg'] > avg_lastHum:
+				hum_avice = f'The average humidity today is higher than the previous {NUM_DAYS} days! (avg: {avg_lastHum})'
+			else:
+				hum_avice = f'The average humidity today is lower than the previous {NUM_DAYS} days! (avg: {avg_lastHum})'
+		except: 
+			'Could not find enough data'
+			AQI_avice = 'not enough data'
+			temp_avice = 'not enough data'
+			hum_avice = 'not enough data'
+
+	elif period == 'week':
+		last_period_date = now + relativedelta(weeks=-1)
+		res = retrieveData(channelID, now, last_period_date)
+		respDEF = calculateStats(res)
+
+		NUM_WEEKS = 4
+		avg_lastAQI = 0
+		avg_lastTemp = 0
+		avg_lastHum = 0
+
+		try: 
+			for d in range(NUM_WEEKS):
+				now = last_period_date
+				last_period_date = now + relativedelta(weeks=-1)
+				res = retrieveData(channelID, now, last_period_date)
+				resp = calculateStats(res)
+				avg_lastAQI += resp['AQI']['avg']
+				avg_lastTemp += resp['temp']['avg']
+				avg_lastHum += resp['hum']['avg']
+			avg_lastAQI /= NUM_WEEKS
+			avg_lastTemp /= NUM_WEEKS
+			avg_lastHum /= NUM_WEEKS
+
+			if respDEF['AQI']['avg'] > avg_lastAQI:
+				AQI_avice = f'The average AQI today is higher than the previous {NUM_WEEKS} weeks! (avg: {avg_lastAQI})'
+			else:
+				AQI_avice = f'The average AQI today is lower than the previous {NUM_WEEKS} weeks! (avg: {avg_lastAQI})'
+			if respDEF['temp']['avg'] > avg_lastTemp:
+				temp_avice = f'The average temperature today is higher than the previous {NUM_WEEKS} weeks! (avg: {avg_lastTemp})'
+			else:
+				temp_avice = f'The average temperature today is lower than the previous {NUM_WEEKS} weeks! (avg: {avg_lastTemp})'
+			if respDEF['hum']['avg'] > avg_lastHum:
+				hum_avice = f'The average humidity today is higher than the previous {NUM_WEEKS} weeks! (avg: {avg_lastHum})'
+			else:
+				hum_avice = f'The average humidity today is lower than the previous {NUM_WEEKS} weeks! (avg: {avg_lastHum})'
+		except:
+			'Could not find enough data'
+			AQI_avice = 'not enough data'
+			temp_avice = 'not enough data'
+			hum_avice = 'not enough data'
+
+
+	elif period == 'month':
+		last_period_date = now + relativedelta(months=-1)
+		res = retrieveData(channelID, now, last_period_date)
+		respDEF = calculateStats(res)
+
+		NUM_MONTHS = 2
+		avg_lastAQI = 0
+		avg_lastTemp = 0
+		avg_lastHum = 0
+
+		try:
+			for d in range(NUM_MONTHS):
+				now = last_period_date
+				last_period_date = now + relativedelta(months=-1)
+				res = retrieveData(channelID, now, last_period_date)
+				resp = calculateStats(res)
+				avg_lastAQI += resp['AQI']['avg']
+				avg_lastTemp += resp['temp']['avg']
+				avg_lastHum += resp['hum']['avg']
+			avg_lastAQI /= NUM_MONTHS
+			avg_lastTemp /= NUM_MONTHS
+			avg_lastHum /= NUM_MONTHS
+
+			if respDEF['AQI']['avg'] > avg_lastAQI:
+				AQI_avice = f'The average AQI today is higher than the previous {NUM_MONTHS} months! (avg: {avg_lastAQI})'
+			else:
+				AQI_avice = f'The average AQI today is lower than the previous {NUM_MONTHS} months! (avg: {avg_lastAQI})'
+			if respDEF['temp']['avg'] > avg_lastTemp:
+				temp_avice = f'The average temperature today is higher than the previous {NUM_MONTHS} months! (avg: {avg_lastTemp})'
+			else:
+				temp_avice = f'The average temperature today is lower than the previous {NUM_MONTHS} months! (avg: {avg_lastTemp})'
+			if respDEF['hum']['avg'] > avg_lastHum:
+				hum_avice = f'The average humidity today is higher than the previous {NUM_MONTHS} months! (avg: {avg_lastHum})'
+			else:
+				hum_avice = f'The average humidity today is lower than the previous {NUM_MONTHS} months! (avg: {avg_lastHum})'
+		except:
+			'Could not find enough data'
+			AQI_avice = 'not enough data'
+			temp_avice = 'not enough data'
+			hum_avice = 'not enough data'
+
+	respDEF['AQI']['Advice'] = AQI_avice
+	respDEF['AQI']['Advice'] = temp_avice
+	respDEF['AQI']['Advice'] = hum_avice
+
+	print(respDEF)
 
