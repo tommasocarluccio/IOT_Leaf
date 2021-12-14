df0cfb77b60fe3be58d395050aea73331f4d9d1d
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2021-12-14 23:31:28 +0100

    Update devices_catalog.py

--- resource catalog/etc/devices_catalog.py
+++ resource catalog/etc/devices_catalog.py
@@ -30,6 +30,7 @@ class DevicesCatalog():
             if device['deviceID']==device_ID:
                 timestamp=time.time()
                 date=datetime.now().strftime("%d/%m/%Y %H:%M")
+                device['resources']=info
                 device['timestamp']=timestamp
                 device['date']=date
                 print("Device {} updated.\n".format(device_ID))
