b83df0e9159ad454bde2611611e4f85d14341402
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2021-12-14 20:45:21 +0100

    Update profiles_catalog.py

--- profiles catalog/profiles_catalog.py
+++ profiles catalog/profiles_catalog.py
@@ -217,7 +217,7 @@ class catalogREST():
                     raise cherrypy.HTTPError(404, "Resource not found")
                 print(output)
             except requests.exceptions.HTTPError as e:
-                return str(e.reason)
+                return str(e.response.reason)
 
                 #raise cherrypy.HTTPError(r_client.status_code)
                 #return r_client
