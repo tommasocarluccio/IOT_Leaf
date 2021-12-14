5c0fd093756d5628f17a05975ea6560f1fd20936
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2021-12-14 17:33:49 +0100

    Update profiles_catalog.py

--- profiles catalog/profiles_catalog.py
+++ profiles catalog/profiles_catalog.py
@@ -199,8 +199,8 @@ class catalogREST():
                     return json.dumps(result)
                 else:
                     output="Resource not found. Can't remove room '{}' from platform '{}'. ".format(room_ID,platform_ID)
-                    raise cherrypy.HTTPError(404,"Platform not found.")
-                #print(output)
+                   raise cherrypy.HTTPError(501, "No operation!")
+                print(output)
             except:
                 return r_client
             
