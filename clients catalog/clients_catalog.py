f140c6b6b41bb4921140a5d8a3145b99e6b2ebb5
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2021-12-15 20:45:09 +0100

    Update clients_catalog.py

--- clients catalog/clients_catalog.py
+++ clients catalog/clients_catalog.py
@@ -37,6 +37,7 @@ class Registration_deployer(object):
                 try:
                     profiles_catalog=self.catalog.retrieveService("profiles_catalog")
                     r=requests.put(profiles_catalog['url']+"/insertProfile",json={"platform_ID":params['platformID']})
+                    print(r)
                     if r.status_code==200:
                         self.catalog.users.removeUser(params['usersID'])
                         self.catalog.platforms.set_value(params['platformID'],"associated",False)
