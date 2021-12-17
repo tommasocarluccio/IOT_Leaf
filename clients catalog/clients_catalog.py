f15427c17b5bb28a587fd1878974a34b141d8432
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2021-12-17 19:20:58 +0100

    Update clients_catalog.py

--- clients catalog/clients_catalog.py
+++ clients catalog/clients_catalog.py
@@ -57,7 +57,7 @@ class Registration_deployer(object):
                 data=self.catalog.users.login(username,password).copy()
                 try:
                     chatID=params['chat_ID']
-                    user=self.users.find_user(username)
+                    user=self.catalog.users.find_user(username)
                     for platform in user['platforms_list']:
                         self.catalog.platforms.add_chatID(platform,chatID)
                     self.catalog.platforms.save()
