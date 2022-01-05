8420f3db84c0299d60a565db3afe5cbc64c3b529
Tommaso Carluccio <tommasocarluccio96@gmail.com>
2022-01-05 18:24:45 +0100

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -928,7 +928,7 @@ class LeafBot(Generic_Service):
             tip=jsonBody["tip"]
             tosend="WARNING!!!\n{} is {} in {} - {}".format(parameter,status,room,platform)
             if tip!=None:
-                tosend+='\nTip: {tip}'
+                tosend+=f'\nTip: {tip}'
             chat_IDs=requests.get(self.clientURL+"/info/"+platform_ID+"/specs/chatIDs").json()
             chat_IDs = list(dict.fromkeys(chat_IDs))
             for chat_ID in chat_IDs:
