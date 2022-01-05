4b0a3b9159d15357988ec4dfbb3eb859bdf841cf
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2022-01-05 22:27:31 +0100

    Update alerting.py

--- controls/telegram_alerting/alerting.py
+++ controls/telegram_alerting/alerting.py
@@ -39,14 +39,11 @@ class AlertingControl(warningControl):
                 parameter=meas['n']
                 try:
                     status=self.compare_value(th_dict[parameter]["min"],th_dict[parameter]["max"],meas['v'])
-                    if status is not False:
-                        room_data=requests.get(self.adaptor_url+'/'+platform_ID+'/'+room_ID+'/now').json()
-                        for key,value in room_data.items():
 
-                            if value[0]==parameter:
-                                last_value=self.compare_value(th_dict[parameter]["min"],th_dict[parameter]["max"],int(value[1]))
-                                v=int(value[1])
-                        if v>20:
+                    if status is not False:
+                        room_data=requests.get(self.adaptor_url+'/'+platform_ID+'/'+room_ID+'/check_warning?parameter='+parameter+"&wnd_size=60").json()
+                        print(room_data)
+                        if last_value is False:
                             msg=self.create_msg(parameter,status)
                             try:
                                 requests.post(self.bot_url+'/warning/'+platform_ID+'/'+room_ID, json=msg)
