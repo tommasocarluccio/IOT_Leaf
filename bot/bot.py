9d644e1b87dc73628eb5c861de92d8714dedfd1e
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2022-01-07 21:13:16 +0100

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -971,10 +971,10 @@ class LeafBot(Generic_Service):
             platform=requests.get(profileURL+"/"+platform_ID+"/platform_name").json()
             room=requests.get(profileURL+"/"+platform_ID+"/rooms/"+room_ID+"/preferences/room_name").json()
             tip=jsonBody["tip"]
-            if status is not "OK":
+            if status!="OK":
                 tosend=":warning:WARNING!!!\n{} is {} in {} - {}".format(parameter,status,room,platform)
             else:
-                tosend=":pray:Good news!!!\n{} is {} in {} - {}".format(parameter,status,room,platform)
+                tosend=":pray:Good news!!!\n{} is {} again in {} - {}".format(parameter,status,room,platform)
             if tip!=None:
                 tosend+=f'\nTip: {tip}'
             chat_IDs=requests.get(self.clientURL+"/info/"+platform_ID+"/specs/chatIDs").json()
