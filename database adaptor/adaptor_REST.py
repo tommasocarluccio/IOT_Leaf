f76509a6c6b3be42c2a4c89844134592322f96b7
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2022-01-15 14:11:34 +0100

    Update adaptor_REST.py

--- database adaptor/adaptor_REST.py
+++ database adaptor/adaptor_REST.py
@@ -95,6 +95,7 @@ class AdaptorREST():
                 clients_catalog=self.adaptor.retrieveService('clients_catalog')
                 clients_result=requests.get(clients_catalog['url']+"/info/"+platform_ID+'/'+room_ID+"/thingspeak").json()
                 client=next((item for item in clients_result if item["room"] == room_ID), False)
+                print(client)
                 channelID=client["channelID"]
                 put_key=client["put_key"]
             except Exception as e:
