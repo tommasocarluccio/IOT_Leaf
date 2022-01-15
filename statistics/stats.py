2395aaa6a20f8dae82fe7165337bc4f2262f2cbc
AndreaAvignone <64846335+AndreaAvignone@users.noreply.github.com>
2022-01-15 12:08:00 +0100

    Update stats.py

--- statistics/stats.py
+++ statistics/stats.py
@@ -113,7 +113,7 @@ class Stats(Generic_Service):
                     avg_last=respDEF[p['name']]['avg_last']
 
                     if avg > avg_last:
-                        respDEF[p['name']['Advice']] = f'The average {p['name']} today is higher than the previous {NUM_DAYS} days! (avg: {round(avg_last,2)})'
+                        respDEF[p['name']['Advice']] = f'The average AQI this week is higher than the previous {NUM_WEEKS} weeks! (avg: {avg_last})'
                     else:
                         respDEF[p['name']['Advice']] = f'The average {p['name']} today is lower than the previous {NUM_DAYS} days! (avg: {round(avg_last,2)})'
 
